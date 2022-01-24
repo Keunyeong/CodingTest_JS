406deb45cbcbd0730363dc17d128205a9fe7ded8
Keunyeong LEE <79135142+Keunyeong@users.noreply.github.com>
2022-01-24 11:13:00 +0900

    Update 8393.js

--- BOJ/for문/8393.js
+++ BOJ/for문/8393.js
@@ -1,1 +1,9 @@
+let fs = require('fs'); //(/dev/stdin)
+let input = fs.readFileSync('input.txt').toString().split('\n');
 
+let num = Number(input[0]);
+let sum = 0;
+for(let i=1; i<num+1; i++){
+  sum += i;
+}
+console.log(sum);
